79df7a275d209fba9c6bf6dff0fc328d176471af
Miguel Higuera <202010202@itla.edu.do>
2023-08-15 23:55:43 -0400

    modify assert in tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,4 +9,4 @@ from app import app
 def test_hello():
     client = app.test_client()
     response = client.get('/')
-    assert b'There is a error here' in response.data
+    assert b'Hi, this is my first app with Flask!' in response.data
